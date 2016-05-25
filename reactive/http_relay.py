6711e4178c71aac4c8889b064c0422b60b06d90c
Casey Marshall <casey.marshall@canonical.com>
2016-05-25 16:47:40 -0500

    Install etcdctl, disable service, simplify config check.

--- reactive/http_relay.py
+++ reactive/http_relay.py
@@ -4,29 +4,17 @@ from charms.reactive import when, when_not, set_state
 @hook('config-changed')
 def config_changed():
     conf = hookenv.config()
-    if not conf.get('name'):
-        hookenv.status_set('error', 'missing required config param "name"')
-        remove_state('relay.available')
-        return
-    if not conf.get('etcd'):
-        hookenv.status_set('error', 'missing required config param "etcd"')
-        remove_state('relay.available')
-        return
+    for k in ('name', 'etcd'):
+        if not conf.get(k):
+            hookenv.status_set('error', 'missing required config param "%s"' % (k))
+            remove_state('relay.available')
+            return
     hookenv.status_set('maintenance', 'configured')
     set_state('relay.available')
 
 
-@when_not('http-relay.installed')
-def install_http_relay():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
-    set_state('http-relay.installed')
+@when_not('etcdctl.installed')
+def install_etcdctl():
+    apt_install(['etcd'])
+    check_call(['systemctl', 'disable', 'etcd'])  # debian package is helpful*
+    set_state('etcdctl.installed')
